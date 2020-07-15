833b6e856d6b2b2c73395bc4b517afecd90aa9ee
sakitkepala <andikapriyotama@gmail.com>
2020-07-15 23:52:49 +0700

    [IMP] Komponen AppointmentsDayView render list appointment

--- src/Appointment.js
+++ src/Appointment.js
@@ -1,5 +1,22 @@
 import React from 'react';
 
+const waktuAppointmentHariItu = mulaiPada => {
+  const [jam, menit] = new Date(mulaiPada).toTimeString().split(':');
+  return `${jam}:${menit}`;
+};
+
 export const Appointment = ({ customer: { namaDepan } }) => (
   <div>{namaDepan}</div>
+);
+
+export const AppointmentsDayView = ({ appointments }) => (
+  <div id="appointmentsDayView">
+    <ol>
+      {appointments.map(appointment => (
+        <li key={appointment.mulaiPada}>
+          {waktuAppointmentHariItu(appointment.mulaiPada)}
+        </li>
+      ))}
+    </ol>
+  </div>
 );
--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Appointment } from '../src/Appointment';
+import { Appointment, AppointmentsDayView } from '../src/Appointment';
 
 describe('Appointment', () => {
   let container;
@@ -12,7 +12,7 @@ describe('Appointment', () => {
 
   const render = komponen => ReactDOM.render(komponen, container);
   
-  it ('nge-render nama depannya customer', () => {
+  it('nge-render nama depannya customer', () => {
     // "Arrange", untuk testing komponen.
     // Di React, berarti juga termasuk mempersiapkan component dan container-nya, dimana komponen akan di-mount
     // Tapi, untuk container-nya sudah didefinisikan di `beforeEach()` di atas.
@@ -26,11 +26,51 @@ describe('Appointment', () => {
     expect(container.textContent).toMatch('Mary');
   })
 
-  it ('nge-render nama depan customer yang satunya', () => {
+  it('nge-render nama depan customer yang satunya', () => {
     customer = { namaDepan: 'Belu' };
 
     render(<Appointment customer={customer} />);
 
     expect(container.textContent).toMatch('Belu');
   })
+});
+
+describe('AppointmentsDayView', () => {
+  let container;
+
+  const hariIni = new Date();
+  const appointments = [
+    { mulaiPada: hariIni.setHours(12, 0) },
+    { mulaiPada: hariIni.setHours(13, 0) }
+  ];
+
+  beforeEach(() => {
+    container = document.createElement('div');
+  });
+
+  const render = komponen => ReactDOM.render(komponen, container);
+
+  it('nge-render sebuah div yang benar id-nya', () => {
+    render(<AppointmentsDayView appointments={[]} />);
+
+    expect(container.querySelector('div#appointmentsDayView')).not.toBeNull();
+  });
+
+  // render untuk element ol & li dipisah testnya sendiri-sendiri.
+  // Yang pertama ini, cuma cek jumlah childrennya ada banyak, lebih dari satu.
+  // Sedangkan satunya, dia memverifikasi apakah kontennya sudah benar.
+  it('nge-render banyak appointment pada element ol', () => {
+    render(<AppointmentsDayView appointments={appointments} />);
+
+    expect(container.querySelector('ol')).not.toBeNull();
+    expect(container.querySelector('ol').children).toHaveLength(2);
+  });
+
+  it('nge-render tiap-tiap appointment pada li', () => {
+    render(<AppointmentsDayView appointments={appointments} />);
+
+    expect(container.querySelectorAll('li')).toHaveLength(2);
+    expect(container.querySelectorAll('li')[0].textContent).toEqual('12:00');
+    expect(container.querySelectorAll('li')[1].textContent).toEqual('13:00');
+  });
 });
