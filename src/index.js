e3bdecaba62e9ac3d3900127bd2c838ac3fa08bf
sakitkepala <andikapriyotama@gmail.com>
2020-07-20 12:55:36 +0700

    [IMP] Render Form Customer di entry point index.js untuk uji coba manual

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppointmentsDayView } from './AppointmentsDayView'
 import { appointmentSampel } from './dataSampel';
+import { FormCustomer } from './FormCustomer';
 
 ReactDOM.render(
-  <AppointmentsDayView appointments={appointmentSampel} />,
+  // <AppointmentsDayView appointments={appointmentSampel} />,
+  <FormCustomer />,
   document.getElementById('root')
 );
